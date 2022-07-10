da0b94eaef497b169a7108544670d6af42d429e7
TechTalkerShubh <jitiya66@gmail.com>
2022-07-10 12:50:20 +0530

    Find the ASCII value of a character

---
+++ src/DS/Sting/C03_ASCII.java
@@ -0,0 +1,10 @@
+package DS.Sting;
+
+public class C03_ASCII {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        int ch2 = 'A';
+        System.out.println((int)ch1);
+        System.out.println(ch2);
+    }
+}
