c35863ae2f40cbd0aac11c41b02492f0792c45f0
TechTalkerShubh <jitiya66@gmail.com>
2022-07-13 21:24:03 +0530

    Find all Palindrome numbers in a given range

---
+++ src/DS/Numbers/C02_all_palindrome.java
@@ -0,0 +1,30 @@
+package DS.Numbers;
+
+public class C02_all_palindrome {
+    public static void main(String[] args) {
+       int start = 10, end = 50;
+       for(int i = start; i <=end; i++)
+           System.out.println(isPalindrome(i));
+    }
+
+    private static boolean isPalindrome(int originalNum) {
+        int reversed = 0;
+        int copyNum = originalNum;
+        while(copyNum > 0){
+
+            //Extract last digit
+            int extract = copyNum % 10;
+            //Append
+            reversed = reversed*10 + extract;
+            //Remove last digit
+            copyNum = copyNum / 10;
+        }
+        if(originalNum == reversed) {
+            System.out.println(originalNum);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
