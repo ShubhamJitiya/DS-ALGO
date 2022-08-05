4d35a0bdf6f107394e67da480a679f70e467dcd9
TechTalkerShubh <jitiya66@gmail.com>
2022-08-05 14:00:26 +0530

    Write a program to find a substring within a string. If found display its starting position

---
+++ src/DS/Sting/C27_find_pos.java
@@ -0,0 +1,10 @@
+package DS.Sting;
+
+public class C27_find_pos {
+    public static void main(String[] args) {
+        String str1 = "takeuforward";
+        String str2 = "forward";
+
+        System.out.println(str1.indexOf(str2));
+    }
+}
